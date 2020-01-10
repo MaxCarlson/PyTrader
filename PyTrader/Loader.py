af8118d49154bdc47a11192c33e95c95c8dd6ea9
mcarls <mcarls@SLICE>
2020-01-09 23:11:29 -0800

    Initial Commit

--- PyTrader/Loader.py
+++ PyTrader/Loader.py
@@ -4,15 +4,15 @@ import csv
 class Loader():
         
     def __init__(self):
-        tickers = {}
+        self.tickers = {}
         return
 
     def load(self, filename='WIKI_PRICES.csv', delimiter=','):
         with open(filename) as csv_file:
             csvReader = csv.reader(csv_file, delimiter=delimiter)
             for line in csvReader:
-                if line[0] in tickers:
-                    tickers[line[0]].append(line[1:])
+                if line[0] in self.tickers:
+                    self.tickers[line[0]].append(line[1:])
                 else:
-                    tickers[line[0]] = [line[1:]]
+                    self.tickers[line[0]] = [line[1:]]
 
