6b6a4e467bb375795edfa3886b7208a57debc32b
mcarls <mcarls@SLICE>
2020-01-13 23:50:56 -0800

    Creating an interface to test strats. Building some simple strats to test interface. Once interface is funcitoning I'll dive into more comlex stratagies

--- PyTrader/Strat.py
+++ PyTrader/Strat.py
@@ -2,16 +2,22 @@
 
 class Strat():
 
-    def __init__(self):
+    def __init__(self, minDays):
+        self.minDays = minDays # Minimum number of days that must be run before applying Strat
         pass
 
-    def run(self):
+    def run(self, close, days):
         pass
 
 
 class BuyAndHold(Strat):
-    def __init__(self):
-        Strat.__init__(self)
+
+    def __init__(self, minDays):
+        Strat.__init__(self, minDays)
+
+    def run(self, close, days):
+        if days != 0:
+            return
 
 
 class DCA(Strat):
@@ -21,9 +27,17 @@ class DCA(Strat):
 
 class MACDStrat(Strat):
 
-    def __init__(self):
-        Strat.__init__(self)
+    def __init__(self, short = 12, long = 26):
+        Strat.__init__(self, long)
+        self.emaLong        = 0
+        self.emaShort       = 0
+        self.longPeriod     = long
+        self.shortPeriod    = short
+        self.longSmoothing  = 2 / (longPeriod + 1)
+        self.shortSmoothing = 2 / (shortPeriod + 1)
+
+    def run(self, close, days):
+        self.emaLong  = (close - self.emaLong)  * self.longSmoothing  + self.emaLong
+        self.emaShort = (close - self.emaShort) * self.shortSmoothing + self.emaShort
 
 
-    def run(self):
-        pass
