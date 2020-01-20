ed2862ea330bc812788fe20f2b49d89756cf486e
MaxCarlson <bart4282819@gmail.com>
2020-01-20 00:19:34 -0800

    Updating Loader.loadCSV function to make use of some pandas functions

--- PyTrader/Loader.py
+++ PyTrader/Loader.py
@@ -19,25 +19,53 @@ class Loader():
         self.fields         = {}
 
     def loadCSV(self, startDate='', daysPerTicker=1000000, filename='', delimiter=','):
-        idx = -1
-        num = 0
-        df  = pd.read_csv(filename)
+        idx     = 0
+        pIdx    = 0
+        num     = 0
+        df      = pd.read_csv(filename)
         prevSymbol = None
+
+
+        while idx < len(df):
+            symbol  = df.iat[0, idx]
+            data    = df.loc[df['ticker'].isin([symbol])]
+            data    = data.drop('ticker', 1)
+            ticker  = Ticker(symbol, data, self.fields, startDate)
+
+            idx     += len(data)
+            if idx >= len(df):
+                break
+
+            if not ticker.isViable(self.dateHandler, daysPerTicker, startDate):
+                continue
+
+            self.tickers[prevSymbol] = ticker
+
+            # Debugging
+            num += 1
+            if num >= 50:
+                break
+
+        '''
         for symbol in df['ticker']:
             idx += 1
             if not prevSymbol:          prevSymbol = symbol
             if symbol == prevSymbol:    continue
 
-            dfTicker    = df.iloc[0:idx-1, 1:-1]
+            dfTicker    = df.iloc[pIdx:idx-1, 1:-1]
             ticker      = Ticker(prevSymbol, dfTicker, self.fields, startDate)
             if not ticker.isViable(self.dateHandler, daysPerTicker, startDate):
                 continue
             self.tickers[prevSymbol] = ticker
-            prevSymbol = symbol
+            pIdx        = idx
+            prevSymbol  = symbol
 
+            # Debugging
             num += 1
             if num >= 50:
                 break
+        '''
+        a = 5
         
     @classmethod
     def loadPickle(self, filename, extension = '.bin'):
